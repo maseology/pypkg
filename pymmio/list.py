8fa07a9efe76c2c1fa0d89e34136e19e5591eedd
maseology <maseology@pm.me>
2022-09-27 09:55:55 -0400

    added list

---
+++ pymmio/list.py
@@ -0,0 +1,6 @@
+
+# https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
